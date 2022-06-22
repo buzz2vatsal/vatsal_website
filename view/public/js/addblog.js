2373980b476092b641e9eef5897c914d6c615f40
buzz2vatsal <vatsalforapple@gmail.com>
2022-06-22 08:41:50 +0530

    Added ckeditor directly to root

--- view/public/js/addblog.js
+++ view/public/js/addblog.js
@@ -1,81 +1,74 @@
-let newEditor ;
-DecoupledEditor
-.create(document.querySelector('#editor'))
-.then(editor => {
-    newEditor  = editor;
-    const toolbarContainer = document.querySelector('#toolbar-container');
-    toolbarContainer.appendChild(editor.ui.view.toolbar.element);
-})
-.catch(error => {
-    console.error(error);
-});
+let newEditor;
+let tagsAdded = false;
+DecoupledDocumentEditor
+    .create(document.querySelector('#editor'))
+    .then(editor => {
+        newEditor = editor;
+        const toolbarContainer = document.querySelector('#toolbar-container');
+        toolbarContainer.appendChild(editor.ui.view.toolbar.element);
+    })
+    .catch(error => {
+        console.error(error);
+    });
 function handleIt() {
-    // console.log(CKEDITOR.instances.editor1.getData(), 'ccc');
-    console.log(document.forms["blogForm"]["imageCaption"]);
-    const title = document.forms["blogForm"]["title"].value;
-    const blogtags = document.forms["blogForm"]["tag"].value;
-    const altText = document.forms["blogForm"]["altText"].value;
-    const imageCaption = document.forms["blogForm"]["imageCaption"].value;
-    const subTitle = document.forms["blogForm"]["subTitle"].value;
-    const specialMention = document.forms["blogForm"]["specialMention"].value;
-    const sequence = document.forms["blogForm"]["sequence"].value;
-    // const editor = document.forms["blogForm"]["editor"].value;
-    console.log(newEditor, 'newEditor');
-    const editorData = newEditor.getData();
-    console.log(editorData, 'editorData');
-    let isreturn = false
-    if (title == ""
-        || imageCaption == ""
-        || altText == ""
-        || subTitle == ""
-        || specialMention == ""
-        || sequence == "") {
-        isreturn = true;
+    if (!tagsAdded) {
+        console.log('2222');
+        // console.log(CKEDITOR.instances.editor1.getData(), 'ccc');
+        console.log(document.forms["blogForm"]["imageCaption"], 'imag');
+        const title = document.forms["blogForm"]["title"].value;
+        const blogtags = document.forms["blogForm"]["tag"].value;
+        const altText = document.forms["blogForm"]["altText"].value;
+        const imageCaption = document.forms["blogForm"]["imageCaption"].value;
+        const subTitle = document.forms["blogForm"]["subTitle"].value;
+        const specialMention = document.forms["blogForm"]["specialMention"].value;
+        const sequence = document.forms["blogForm"]["sequence"].value;
+        // const editor = document.forms["blogForm"]["editor"].value;
+        console.log(newEditor, 'newEditor');
+        const editorData = newEditor.getData();
+        console.log(editorData, 'editorData');
+        let isreturn = false
+        if (title == ""
+            || imageCaption == ""
+            || altText == ""
+            || subTitle == ""
+            || specialMention == ""
+            || sequence == "") {
+            isreturn = true;
+        }
+        if (!tagarrary.length) {
+            alert("Tage must be filled out");
+            isreturn = true;
+        }
+        console.log(tagarrary, 'blogtags');
+        if (isreturn) {
+            alert("Please fill all required field");
+            return;
+        }
+        else {
+            addBlogApi(
+                {
+                    thumbnail_image: 'https://reactjs.org/logo-og.png',
+                    image_caption: imageCaption,
+                    alt_text: altText,
+                    title: title,
+                    sub_title: subTitle,
+                    content: editorData,
+                    tags: tagarrary,
+                    special_mention: specialMention,
+                    is_blog: 1,
+                    sequence: sequence
+                }
+            );
+        }
+    } else {
+        tagsAdded = false;
     }
-    // if (blogtags == "") {
-    //     alert("Tage must be filled out");
-    //     isreturn = true;
-    // }
-    if (isreturn) {
-        alert("Please fill all required field");
-        return true;
-    }
-    else {
-        // console.log(editor, 'editor');
-        console.log(
-            {
-                        thumbnail_image: 'https://reactjs.org/logo-og.png',
-                        image_caption: imageCaption,
-                        alt_text: altText,
-                        title: title,
-                        sub_title: subTitle,
-                        // content text,
-                        special_mention: specialMention,
-                        is_blog: true,
-                        sequence: sequence
-                    }
-        );
-        addBlogApi(
-            {
-                thumbnail_image: 'https://reactjs.org/logo-og.png',
-                image_caption: imageCaption,
-                alt_text: altText,
-                title: title,
-                sub_title: subTitle,
-                content: editorData,
-                special_mention: specialMention,
-                is_blog: true,
-                sequence: sequence
-            }
-        );
-    }
-
 }
 
 APIURL = 'https://oi7nrcfmd6.execute-api.ap-south-1.amazonaws.com';
 // APIURL = 'http://localhost:3000';                                        
 const addBlogApi = async (apiData) => {
-    const response = await fetch(`${APIURL}/addblog`,{
+    const response = await fetch(`${APIURL}/addblog`, {
         method: 'POST',
         body: JSON.stringify(apiData)
         // headers:{
@@ -86,13 +79,15 @@ const addBlogApi = async (apiData) => {
     });
     const apiResponse = await response.json();
     console.log(apiResponse, 'api respp');
+    if (apiResponse) {
+        window.location.href = window.location.origin + '/blog.html';
+    }
 }
 
 
 
 const tagelement = document.getElementById("tag");
 tagelement.addEventListener("keydown", function (e) {
-
     if (e.keyCode == 13) {
         addtotagarray();
     }
@@ -100,6 +95,7 @@ tagelement.addEventListener("keydown", function (e) {
 
 const tagarrary = []
 function addtotagarray() {
+    tagsAdded = true;
     console.log(tagelement.value)
     if (!tagarrary.includes(tagelement.value)) {
         tagarrary.push(tagelement.value)
--- view/public/js/blog.js
+++ view/public/js/blog.js
@@ -1,7 +1,7 @@
 const APIURL = 'https://oi7nrcfmd6.execute-api.ap-south-1.amazonaws.com';
 
-const fetchAllBlogs = async () => {
-    const response = await fetch(`${APIURL}/fetchallblog`, {
+const fetchAllBlogs = async (params) => {
+    const response = await fetch(`${APIURL}/fetchallblog?tags=${params ? params : ''}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -11,11 +11,50 @@ const fetchAllBlogs = async () => {
     console.log(apiResponse, 'api respp');
     appendBlogs(apiResponse);
 }
+
+const fetchAllTags = async (params) => {
+    const response = await fetch(`${APIURL}/getTags`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const apiResponse = await response.json();
+    const allTags = [];
+    apiResponse.tags.map((eachTag) => {
+        allTags.push(eachTag[0]);
+    });
+    console.log(allTags, 'allTags');
+    appendTags(allTags, params)
+}
+
 function listBlogs() {
-    fetchAllBlogs();
+    const params = window.location.search || '';
+    queryString = params.split("?tags=")[1];
+    console.log(queryString, 'queryStringqueryString');
+    fetchAllTags(queryString);
+    fetchAllBlogs(queryString);
 }
 listBlogs();
 
+const appendTags = (allTags, params) => {
+    const tagsDiv = document.getElementById("bl_tag");
+    const tagListAll = document.createElement('li');
+    tagListAll.className = params ? 'pointer' : 'active pointer';
+    tagListAll.innerHTML = `<a onclick="tags('')" >all</a>`;
+    console.log(tagListAll, params, 'aaaaa');
+    tagsDiv.appendChild(tagListAll);
+    if (params) {
+        params = params.split(',%20');
+    }
+    console.log(params, 'paramsparams');
+    allTags.forEach((eachTags) => {
+        const tagList = document.createElement('li');
+        tagList.className = params && params.includes(eachTags) ? 'active pointer' : 'pointer';
+        tagList.innerHTML = `<a onclick="tags('${eachTags}')">${eachTags}</a>`;
+        tagsDiv.appendChild(tagList);
+    });
+}
 
 const appendBlogs = (listBlogs) => {
     const blogDiv = document.getElementById("blogList");
@@ -47,3 +86,24 @@ const appendBlogs = (listBlogs) => {
     });
 }
 
+function tags(tags) {
+    console.log(tags, 'tags');
+    console.log(window.location, 'tags');
+    if(!tags) {
+        console.log('11111111', window.location.origin + window.location.pathname);
+        window.location.href = window.location.origin + window.location.pathname;
+    } else {
+        if (window.location.search === '') {
+            window.location.href = window.location.href + '?tags=' + tags;
+        } else {
+            console.log(window.location.search);
+            queryString = window.location.search.split("?tags=")[1];
+            params = queryString.split(',%20');
+            if (params.includes(tags)) {
+                console.log(params, tags, 'includes');
+            } else {
+                window.location.href = window.location.search + ', ' + tags;
+            }
+        }
+    }
+}
